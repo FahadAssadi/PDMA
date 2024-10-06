2aa7503dcbed69913d66fd6ad6f19fa95fd30a4c
Fahad Assadi <fass0001@student.monash.edu>
2024-10-06 17:25:10 +1100

    Added an appropriate home page

--- PDMA/src/app/core/home/home.component.ts
+++ PDMA/src/app/core/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
