f9a53f758abf28ca8da786ccc5d342d7bf679c6f
Fahad Assadi <fass0001@student.monash.edu>
2024-10-07 22:45:06 +1100

    Updated the header to have some drop downs

--- PDMA/src/app/core/home/home.component.ts
+++ PDMA/src/app/core/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
