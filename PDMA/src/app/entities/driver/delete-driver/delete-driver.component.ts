4be6465ee6c55462570b414586888cb112b46d63
Fahad Assadi <fass0001@student.monash.edu>
2024-10-09 12:32:36 +1100

    Updateing delete driver to use the new table template

--- PDMA/src/app/entities/driver/delete-driver/delete-driver.component.ts
+++ PDMA/src/app/entities/driver/delete-driver/delete-driver.component.ts
@@ -3,34 +3,59 @@ import { DatabaseService } from '../../../shared/services/database/database.serv
 import { StringToUpperPipe } from '../../../shared/pipes/string-to-upper/string-to-upper.pipe';
 
 import { Driver } from '../../../shared/models/Driver';
+import { TableHeader, TableAction } from '../../../shared/models/models';
+import { TableTemplateComponent } from '../../../shared/templates/table-template/table-template.component';
 
 
 @Component({
   selector: 'app-delete-driver',
   standalone: true,
-  imports: [ StringToUpperPipe],
+  imports: [ StringToUpperPipe, TableTemplateComponent],
   templateUrl: './delete-driver.component.html',
   styleUrl: './delete-driver.component.css'
 })
 export class DeleteDriverComponent {
   drivers: Driver[] = [];
 
+  // Table Related Info
+  tableHeaders: TableHeader[] = [];
+  tableData: any[] = [];
+  tableActions: TableAction[] = [];
+
   constructor(private db: DatabaseService) {} 
 
   ngOnInit(): void {
     this.getDrivers();
+
+    // Update table data
+    this.tableHeaders = [
+      { key: 'driverId', label: 'ID' },
+      { key: 'driverName', label: 'Name' },
+      { key: 'driverDepartment', label: 'Department' },
+      { key: 'driverLicense', label: 'License' },
+      { key: 'driverIsActive', label: 'Is Active' },
+      { key: 'driverCreatedAt', label: 'Created At' }
+    ];
+    this.tableActions = [
+      {
+        label: 'X',
+        style: 'btn btn-danger',
+        function: (driver: Driver) => this.deleteDriver(driver._id)
+      }
+    ];
   }
 
   getDrivers(): void {
     this.db.getDrivers().subscribe((drivers) => {
       this.drivers = drivers;
+
+      this.tableData = this.drivers;
     });
   }
 
   deleteDriver(driverId: string): void {
-    // Delete driver from database
-    this.db.deleteDriver(driverId).subscribe((data) => {
-      this.drivers = this.drivers.filter((driver) => driver._id !== driverId);
+    this.db.deleteDriver(driverId).subscribe(() => {
+      this.getDrivers();
     });
   }
 }
--- PDMA/src/app/entities/package/delete-package/delete-package.component.ts
+++ PDMA/src/app/entities/package/delete-package/delete-package.component.ts
@@ -3,33 +3,57 @@ import { DatabaseService } from '../../../shared/services/database/database.serv
 import { KgToGPipe } from '../../../shared/pipes/kg-to-g/kg-to-g.pipe';
 
 import { Package } from '../../../shared/models/Package';
+import { TableHeader, TableAction } from '../../../shared/models/models';
+import { TableTemplateComponent } from '../../../shared/templates/table-template/table-template.component';
 
 @Component({
   selector: 'app-delete-package',
   standalone: true,
-  imports: [KgToGPipe],
+  imports: [KgToGPipe, TableTemplateComponent],
   templateUrl: './delete-package.component.html',
   styleUrl: './delete-package.component.css'
 })
 export class DeletePackageComponent {
   packages: Package[] = [];
 
+  // Table Related Info
+  tableHeaders: TableHeader[] = [];
+  tableData: any[] = [];
+  tableActions: TableAction[] = [];
+
   constructor(private db: DatabaseService) {}
 
   ngOnInit(): void {
     this.getPackages();
+
+    // Update table data
+    this.tableHeaders = [
+      { key: 'packageId', label: 'ID' },
+      { key: 'packageName', label: 'Name' },
+      { key: 'packageWeight', label: 'Weight' },
+      { key: 'packageDestination', label: 'Destination' },
+      { key: 'packageIsActive', label: 'Is Active' },
+      { key: 'packageCreatedAt', label: 'Created At' }
+    ];
+    this.tableActions = [
+      {
+        label: 'X',
+        style: 'btn btn-danger',
+        function: (pkg: Package) => this.deletePackage(pkg._id)
+      }
+    ];
   }
 
   getPackages(): void {
     this.db.getPackages().subscribe((packages) => {
       this.packages = packages;
+      this.tableData = this.packages;
     });
   }
 
   deletePackage(packageId: string): void {
-    // Delete package from database
-    this.db.deletePackage(packageId).subscribe((data) => {
-      this.packages = this.packages.filter((pkg) => pkg._id !== packageId);
+    this.db.deletePackage(packageId).subscribe(() => {
+      this.getPackages();
     });
   }
 }
